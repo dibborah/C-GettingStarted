974680a148d766daa9df89d1b275c4f2cd5f9e19
dibya.borah <dibyajyotiborah469@gmail.com>
2025-05-14 19:25:11 +0530

    Done 2D arrays

--- MySecondProject/TempApp/Program.cs
+++ MySecondProject/TempApp/Program.cs
@@ -427,3 +427,33 @@
 //         }
 //     }
 // }
+
+// Learning 2D arrays
+using System;
+
+namespace TwoDArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] numberGrid = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+                {10,11,12},
+            };
+
+            int [,] myArray = new int[2,3];// Two arrays inside with 3 elements each
+
+            myArray[0,0] = 1;
+            myArray[0,1] = 2;
+            myArray[0,2] = 3;
+            Console.WriteLine(myArray[0,0]);
+            Console.WriteLine(myArray[0,1]);
+            Console.WriteLine(myArray[0,2]);
+            // Console.WriteLine(myArray[0,3]);
+            Console.ReadLine();
+        }
+    }
+}
--- MySecondProject/TempApp/obj/Debug/net9.0/TempApp.AssemblyInfo.cs
+++ MySecondProject/TempApp/obj/Debug/net9.0/TempApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e133bb6ce42ce3298861b4f76d4df176b6f4ad7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1274a3830dd674b2820a4efc1941c3dc70cf83")]
 [assembly: System.Reflection.AssemblyProductAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
